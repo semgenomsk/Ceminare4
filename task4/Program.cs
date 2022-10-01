78e32f6fb9e964dc7420db52d1d4402ac8fa2447
Semen <semgenomsk@yandex.ru>
2022-10-01 15:06:54 +0600

    Done task 30

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int [] arr=new int[8];
+
+Random rand=new Random();
+for(int i=0; i<arr.Length; i++)
+{
+    arr[i]=rand.Next(0,2);
+}
+
+Console.WriteLine(string.Join(", ",arr));
